b03ae45341a5b11fbd5606f58c7aa46c45583566
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-11-12 14:59:49 +0300

    Добавил отправку тестового файла

--- backend/uemployees/export/views.py
+++ backend/uemployees/export/views.py
@@ -1,11 +1,31 @@
-import io
+import os
 
 from flask_restful import (Resource)
-from flask import (send_from_directory,send_file)
+from flask import (send_from_directory, send_file)
 from openpyxl import Workbook
 
 
 class ExportView(Resource):
     def get(self):
+        THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+        my_file = os.path.join(THIS_FOLDER, 'test.xlsx')
 
-        return "Тут должне отправляться файл через один из методов send_from_directory, send_file"
+        wb = Workbook()
+        faculty = wb.create_sheet("faculty")
+        faculty['A1'] = "id"
+        faculty['B1'] = "name"
+
+        wb.save(my_file)
+
+        response = send_file(
+            my_file,
+            as_attachment=True,
+            attachment_filename="test.xlsx",
+            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+
+        response.headers["Content-Length"] = os.path.getsize(my_file)
+        response.headers["Content-Disposition"] = "attachment; filename=test.xlsx"
+        response.headers["Content-Type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+        return response
